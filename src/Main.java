9b79ec6ef1d4ebf24f8d720d929c2ebb0513ec5c
Amax-23 <maksim.agoshkov@gmail.com>
2022-07-22 10:53:50 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         Terminal terminal = new Terminal();
 
         System.out.println("Начинаем работать с Git в командной строке");
+        System.out.println("Финальные изменения по слиянию веток");
     }
 }
