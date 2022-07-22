b52cf57acc8b74dc4051f6968914ab9de33f0637
Amax-23 <maksim.agoshkov@gmail.com>
2022-07-22 10:38:14 +0300

    Terminal page

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Terminal terminal = new Terminal();
 
         System.out.println("Начинаем работать с Git в командной строке");
     }
